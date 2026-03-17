6465857bdd7e1472ebfd363d35327a66cd34ae08
peterw23109 <23109@burnside.school.nz>
2026-03-17 13:33:46 +1300

    added a home html

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, g, render_template
+import sqlite3
+
+DATABASE = 'element.db'
+
+app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+        
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.route('/')
+def home():
+    sql = "SELECT * FROM Element"
+    result = query_db(sql)
+    return render_template("home.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
