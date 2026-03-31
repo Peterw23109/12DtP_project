dd5c6aa05b9809bf84f24d5c10384729e381d68e
peterw23109 <23109@burnside.school.nz>
2026-04-01 11:11:35 +1300

    added homepage(gonna work on login soon)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, g, render_template, request
+from flask import Flask, g, render_template, request, flash, session, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 
 DATABASE = 'element.db'
@@ -24,8 +25,29 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
-@app.route('/', methods=["GET", "POST"])
-def home():
+@app.route('/signup', methods=["GET","POST"])
+def signup():
+    #if the user posts from the signup page
+    if request.method == "POST":
+        #add the new username and hashed password to the database
+        username = request.form['username']
+        password = request.form['password']
+        #hash it with the cool secutiry function
+        hashed_password = generate_password_hash(password)
+        #write it as a new user to the database
+        sql = "INSERT INTO user (username,password) VALUES (?,?)"
+        query_db(sql,(username,hashed_password))
+        #message flashes exist in the base.html template and give user feedback
+        flash("Sign Up Successful")
+    return render_template('signup.html')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/quiz', methods=["GET", "POST"])
+def quiz():
     result = []
     
 
@@ -52,7 +74,7 @@ def home():
             random_element = query_db("SELECT Element_ID FROM Element ORDER BY RANDOM() LIMIT 1", one=True)
             RANDOM_SYMBOL = random_element['Element_ID']
 
-            return render_template("result.html", random_symbol=Element_ID)
+            return render_template("element.html", random_symbol=Element_ID)
 
 
         sql = "SELECT * FROM Element WHERE Element_ID = ? COLLATE NOCASE"
@@ -64,7 +86,41 @@ def home():
         result = []
 
 
-    return render_template("home.html", result=result, random_symbol=RANDOM_SYMBOL)
+    return render_template("element.html", result=result, random_symbol=RANDOM_SYMBOL)
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    #if the user posts a username and password
+    if request.method == "POST":
+        #get the username and password
+        username = request.form['username']
+        password = request.form['password']
+        #try to find this user in the database- note- just keepin' it simple so usernames must be unique
+        sql = "SELECT * FROM user WHERE username = ?"
+        user = query_db(sql=sql,args=(username,),one=True)
+        if user:
+            #we got a user!!
+            #check password matches-
+            if check_password_hash(user[2],password):
+                #we are logged in successfully
+                #Store the username in the session
+                session['user'] = user
+                flash("Logged in successfully")
+            else:
+                flash("Password incorrect")
+        else:
+            flash("Username does not exist")
+    #render this template regardles of get/post
+    return render_template('login.html')
+
+
+
+@app.route('/logout')
+def logout():
+    #just clear the username from the session and redirect back to the home page
+    session['user'] = None
+    return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
