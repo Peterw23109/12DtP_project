3722dc46b5b9380350d8265dd4f2f34e1c196a03
peterw23109 <23109@burnside.school.nz>
2026-04-13 09:54:14 +1200

    I deleted login(gonna do it later)

--- app.py
+++ app.py
@@ -25,21 +25,6 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
-@app.route('/signup', methods=["GET","POST"])
-def signup():
-    #if the user posts from the signup page
-    if request.method == "POST":
-        #add the new username and hashed password to the database
-        username = request.form['username']
-        password = request.form['password']
-        #hash it with the cool secutiry function
-        hashed_password = generate_password_hash(password)
-        #write it as a new user to the database
-        sql = "INSERT INTO user (username,password) VALUES (?,?)"
-        query_db(sql,(username,hashed_password))
-        #message flashes exist in the base.html template and give user feedback
-        flash("Sign Up Successful")
-    return render_template('signup.html')
 
 @app.route('/')
 def index():
@@ -88,41 +73,6 @@ def quiz():
 
     return render_template("element.html", result=result, random_symbol=RANDOM_SYMBOL)
 
-@app.route('/login', methods=["GET","POST"])
-def login():
-    #if the user posts a username and password
-    if request.method == "POST":
-        #get the username and password
-        username = request.form['username']
-        password = request.form['password']
-        #try to find this user in the database- note- just keepin' it simple so usernames must be unique
-        sql = "SELECT * FROM user WHERE username = ?"
-        user = query_db(sql=sql,args=(username,),one=True)
-        if user:
-            #we got a user!!
-            #check password matches-
-            if check_password_hash(user[2],password):
-                #we are logged in successfully
-                #Store the username in the session
-                session['user'] = user
-                flash("Logged in successfully")
-            else:
-                flash("Password incorrect")
-        else:
-            flash("Username does not exist")
-    #render this template regardles of get/post
-    return render_template('login.html')
-
-
-
-@app.route('/logout')
-def logout():
-    #just clear the username from the session and redirect back to the home page
-    session['user'] = None
-    return redirect('/')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
